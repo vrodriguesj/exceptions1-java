70b4281087e3bc1cc7b82f6628759c0b6d3498e9
Vinicius Rodrigues <vrodrigues2112@gmail.com>
2020-01-24 12:38:18 -0300

    Versao muito ruim

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,59 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+	
+	private int roomNumber;
+	private Date checkIn;
+	private Date checkOut;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation(int roomNumber, Date chekIn, Date checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = chekIn;
+		this.checkOut = checkOut;
+	}
+
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(int roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getChekIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}	
+	
+	public long duration() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public void updateDate(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		return "Room "
+				+ roomNumber
+				+ ", check-in: "
+				+ sdf.format(checkIn)
+				+ ", check-out: "
+				+ sdf.format(checkOut)
+				+ ", "
+				+ duration()
+				+ " nights";
+	}
+}
